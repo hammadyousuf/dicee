9f8fbd3e99d7980ecbd6c85eebd3f51b117924de
hammad <hammadbhat126@gmail.com>
2022-08-02 18:56:26 +0400

    funcntion

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber= 5;
     return Center(
       child: Row(
         children: <Widget>[
@@ -25,7 +26,7 @@ class DicePage extends StatelessWidget {
           Expanded(
             child:Padding(
             padding: EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png')
+              child: Image.asset('images/dice$leftDiceNumber.png')
 
           ),
           ),
